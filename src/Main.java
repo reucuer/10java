026e7f5afa9b2a9230f37d67cd2958b8b852b1dc
reucuer <reucuer@gmail.com>
2022-07-20 17:10:57 +0800

    5_commit

--- src/Main.java
+++ src/Main.java
@@ -25,13 +25,10 @@ public class Main {
         double B3 = D/A;
         double p = (-1*B1*B1)/3 + B2;
         double q = 2*B1*B1*B1/27 - B1*B2/3 + B3;
-        double delta = -4*B*B*B*D+B*B*C*C -4*A*C*C*C+18*A*B*C*D-27*A*A*D*D;
-        double S = q*q/4 + p*p*p/27;
-        double m = -1*q/2;
-        double n = S;
-        double y = Math.cbrt(m+Math.sqrt(n))+Math.cbrt(m-Math.sqrt(n));
-        double y1 = 2*Math.cbrt(-q/2);
-        double y2 = Math.cbrt(-q/2);
+        double a = -1*q/2;
+        double b = Math.sqrt(q*q/4 + p*p*p/27);
+        double y = Math.cbrt(a + b) + Math.cbrt(a - b);
+
         double x = y - B1/3;
 
         if (delta > 0) answer = "tri_kornya" + '\n' + x + '\n' + x1;
