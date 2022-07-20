e5665fc86ac1c3fddaafad82ef7b572eece0a497
reucuer <reucuer@gmail.com>
2022-07-20 17:02:41 +0800

    Add someONE code

--- src/Main.java
+++ src/Main.java
@@ -11,40 +11,76 @@ import java.lang.Math;
 
 
 public class Main {
+    public static void bubbleSort(int[] arr){
+        for(int i = arr.length-1 ; i > 0 ; i--){
+            for(int j = 0 ; j < i ; j++){
+            if( arr[j] > arr[j+1] ){
+                int tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+            }
+        }
+    }
+}
     public static void main(String[] args) throws FileNotFoundException, IOException {
         String path = "INPUT.TXT";
         File file = new File(path);
         Scanner sc = new Scanner(file);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        int C = sc.nextInt();
-        int D = sc.nextInt();
-        //int t1 = w1 - p/w1;
-        //int t2 = w2 - p/w2;
-        //int t3 = w3 - p/w3;
-        int Q = (A*A-3*B)/9;
-        int R = (2*A*A*A-9*A*B+27*C)/54;
-        String answer;
-        if ((R*R) < (Q*Q*Q)) {
-            double t = Math.acos(R / Math.sqrt(Q * Q * Q)) / 3;
-            double x1 = -2*Math.sqrt(Q)*Math.cos(t)-A/3;
-            double x2 = -2*Math.sqrt(Q)*Math.cos(t+(2*Math.PI/3))-A/3;
-            double x3 = -2*Math.sqrt(Q)*Math.cos(t-(2*Math.PI/3))-A/3;
-            answer = String.valueOf(x1) + String.valueOf(x2) + String.valueOf(x3);
+        double AA = sc.nextDouble();
+        double A = sc.nextDouble()/AA;
+        double B = sc.nextDouble()/AA;
+        double C = sc.nextDouble()/AA;
+        int[] x_mas = new int[3];
+        int count = 0;
+        String answer = "";
+        double Q = (3*B - A*A)/9;
+        double R = (9*A*B - 2*A*A*A - 27*C)/54;
+        double S = Q*Q*Q + R*R;
+        if (S < 0) {
+            count = 3;
+            double phi = Math.acos(R/Math.sqrt((-Q)*(-Q)*(-Q)))/3;
+            x_mas[0] = (int)Math.round(2 * Math.sqrt(-1*Q)*Math.cos(phi) - A/3);
+            x_mas[1] = (int)Math.round(2 * Math.sqrt(-1*Q)*Math.cos(phi+2*Math.PI/3) - A/3);
+            x_mas[2] = (int)Math.round(2 * Math.sqrt(-1*Q)*Math.cos(phi-2*Math.PI/3) - A/3);
         }
-        else{
-            double BB;
-            double AA = -Math.signum(R)*Math.pow(Math.abs(R)+Math.sqrt(R*R-Q*Q*Q),1/3);
-            if (AA != 0) BB = Q/AA;
-            else BB = 0;
-            double x1=(AA+BB)-A/3;
-            if (AA == BB) {
-                double x2= -AA-A/3;
-                answer = String.valueOf(x1) + String.valueOf(x2);
+
+        if (S > 0) {
+            count = 1;
+            double phi = (1/Math.cos(Math.abs(R)/Math.sqrt(Q*Q*Q)))/3;
+            if (Q > 0){
+                x_mas[0] = (int)Math.round(-2 * Math.signum(R)*Math.sqrt(Q)*Math.cosh(phi) - A/3);
             }
-            else answer = String.valueOf(x1);
+            if (Q < 0){
+                x_mas[0] = (int)Math.round(-2 * Math.signum(R)*Math.sqrt(Q)*Math.sinh(phi) - A/3);
+            }
+            if (Q == 0){
+                x_mas[0] = (int)Math.round(-1*Math.cbrt(C - A*A*A/27) - A/3);
+            }
+        }
+        if (S == 0){
+            count = 2;
+            x_mas[0] = (int)(-2 * Math.pow(R, 1/3) - A/3);
+            x_mas[0] = (int)(-2 * Math.signum(R)*Math.sqrt(Q) - A/3);
+            //double d = -2 * Math.cbrt(R) - A/3;
+            //System.out.println(R);
+            //System.out.println(A);
+            //System.out.println(A/3);
+            //System.out.println(-2*Math.cbrt(R));
+            //System.out.println(R);
+            x_mas[1] = (int)Math.round(Math.cbrt(R) - A/3);
+            x_mas[1] = (int)(Math.signum(R)*Math.sqrt(Q) - A/3);
+            System.out.print(R+" ");
+            System.out.println(Math.sqrt(Q));
+            //System.out.println(Math.signum(R));
 
         }
+
+        bubbleSort(x_mas);
+        if (count == 3) answer = answer = x_mas[0] + " " + x_mas[1]+ " " + x_mas[2];
+        if (count == 2) answer = x_mas[0] + " " + x_mas[1];
+        if (count == 1) answer = Integer.toString(x_mas[0]);
+        System.out.println(answer);
+
 
 
 
