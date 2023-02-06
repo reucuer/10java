ca85d9bb08207a768b3660a51b05c1acc361649a
reucuer <reucuer@gmail.com>
2023-02-06 21:07:31 +0800

    7_commit

--- src/Main.java
+++ src/Main.java
@@ -8,90 +8,91 @@ import java.lang.String;
 import java.util.Arrays;
 import java.lang.Math;
 
-//надо вычислить один действительный корень. он вроде всегда есть
-//надо разделить програмно многочлен на многочлен и получится квадратное уравнение
 
 
 public class Main {
-    public static int[] mnoj_by_A(int A) {
-        int Abc_A = Math.abs(A);
-        boolean AA[] = new boolean[Abc_A+1];
-        int AAA[] = new int[Abc_A];
-        int count = 0;
-        for (int i = 1; i <= Abc_A; i++){
-            AA[i] = false;
-            if (Abc_A % i == 0) {
-                AA[i] = true;
-                count++;
-            }
-        }
-        int[] myArray = new int[count];
-        int j = 0;
-        for (int i = 1; i <= Abc_A; i++) {
-            if (AA[i] == true) {
-                myArray[j] = (int)Math.signum(A)*i;
-                j++;
-            }
-        }
-        return myArray;
-    }
-    public static void print(int A[]){
-        int count = A.length;
-         for(int i = 0; i < count; i++){
-            System.out.println(A[i]);
-        }
-    }
     public static void main(String[] args) throws FileNotFoundException, IOException {
         String path = "INPUT.TXT";
         File file = new File(path);
         Scanner sc = new Scanner(file);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        int C = sc.nextInt();
-        int D = sc.nextInt();
+        double A = (double)sc.nextInt();
+        double B = (double)sc.nextInt();
+        double C = (double)sc.nextInt();
+        double D = (double)sc.nextInt();
         String answer = "";
-        int dis, x = 0, cor_dis, x_1 = 0, x_2 = 0;
-        dis = B*B - 4*A*C;
-        cor_dis = (int)Math.sqrt(dis);
-        if (D == 0) {
-            if (dis == 0){
-                x_1 = -B/2*A;
-            }
-            if (dis > 0){
-                x_1 = (-B + cor_dis)/(2*A);
-                x_2 = (-B - cor_dis)/(2*A);
-            }
-
-            if (x == x_1 || x == x_2) dis = 0;
-            if (dis > 0) answer = String.valueOf(x) + ' ' + String.valueOf(x_1) + ' ' + String.valueOf(x_2);
-            if (dis == 0) answer = String.valueOf(x) + ' ' + String.valueOf(x_1);
-
-        } else{
-            int A_mas[] = mnoj_by_A(A);
-            //print(A_mas);
-            int B_mas[] = mnoj_by_A(D);
-            //print(B_mas);
-            int int_count = A_mas.length*B_mas.length;
-            int new_mas[] = new int[int_count];
+        double p=(3*A*C-B*B)/(3*A*A);
+        double q=((2*B*B*B-9*A*B*C+27*A*A*D))/(27*A*A*A);
+        double s=(((q*q)/4)+(p*p*p)/27);
+        double f;
 
 
-                for(int i = 0,j = 0; j < A_mas.length; j++) {
-                    for (int k = 0; k < B_mas.length; k++, i++) {
-                        new_mas[i] = A_mas[j] / B_mas[k];
-                        System.out.println(A_mas[j] / B_mas[k]);
-                    }
-                }
+        double discrim = 4*B*B - 12*A*C;
+        double toch_exstrem_X1;
+        double toch_exstrem_X2;
+        int vetka = -1;
+        if (discrim > 0){
+            toch_exstrem_X1 = (-2*B + Math.sqrt(discrim))/(6*A);
+            toch_exstrem_X2 = (-2*B - Math.sqrt(discrim))/(6*A);
+            //System.out.println(toch_exstrem_X1 + " toch_exstrem_X1");
+            //System.out.println(toch_exstrem_X2 + " toch_exstrem_X2");
+            double f_x1 = A*Math.pow(toch_exstrem_X1, 3) + B*Math.pow(toch_exstrem_X1, 2) + C*Math.pow(toch_exstrem_X1, 1) + D;
+            double f_x2 = A*Math.pow(toch_exstrem_X2, 3) + B*Math.pow(toch_exstrem_X2, 2) + C*Math.pow(toch_exstrem_X2, 1) + D;
+            if (f_x1 * f_x2 < 0) vetka = 3;
+            if (f_x1 * f_x2 == 0) vetka = 2;
+        }
+        else vetka = 1;
 
-            System.out.println("--------");
-                print(A_mas);
-            System.out.println("--------");
-            print(B_mas);
-            System.out.println("--------");
-            print(new_mas);
+        System.out.println(vetka + " kornya:");
+        System.out.println(s + " s:");
+        System.out.println(p + " p:");
+        System.out.println(q + " q:");
+        System.out.println(A + " A:");
+        System.out.println(B + " B:");
+        System.out.println(C + " C:");
+        System.out.println(D + " D:");
+
+
+        if (q < 0){
+            f = (Math.atan(Math.pow(-s,0.5)/(-q/2)));
+            System.out.println(f + ": 1f");
+        }
+        if (q > 0){
+            f = Math.atan(Math.pow(-s,0.5)/(-q/2)) + Math.PI;
+            System.out.println(f + ": 2f");
+        }
+        else {
+            f = Math.PI/2;
+            System.out.println(f + ": 3f");
+        }
+
+        int x1, x2, x3;
+        double x2i, x3i;
+
+        if (s<0)
+        {
+            x1=(int)Math.round(2*Math.sqrt(-p/3)*Math.cos(f/3)-B/3*A);
+            x2=(int)Math.round(2*Math.sqrt(-p/3)*Math.cos(f/3+(2*Math.PI)/3)-B/3*A);
+            x3=(int)Math.round(2*Math.sqrt(-p/3)*Math.cos(f/3+(4*Math.PI)/3)-B/3*A);
+            answer = x1 + " " + x2 + " " + x3;
+        }
+        else if (s>0)
+        {
+            x1=(int)(Math.cbrt(-q/2+Math.pow(s,0.5))+Math.cbrt(-q/2-(Math.pow(s,0.5)))-B/(3*A));
+            answer = String.valueOf(x1);
+
+        }
+        else
+        {
+            x1=(int)Math.round(2*Math.cbrt(-q/2)-B/(3*A));
+            x2=(int)Math.round(-1*Math.cbrt(-q/2)-B/(3*A));
+            answer = x1 + " " + x2;
         }
 
 
+
         System.out.println(answer);
+
+
 
         FileWriter writerS = new FileWriter("OUTPUT.TXT", false);
         writerS.write(String.valueOf(answer));
