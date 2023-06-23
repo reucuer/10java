503b20b732ae4681476599189b3e99d53fdc6ccc
reucuer <reucuer@gmail.com>
2023-06-23 19:31:58 +0800

    8-Otn pravil commit

--- src/Main.java
+++ src/Main.java
@@ -7,91 +7,110 @@ import java.util.Scanner;
 import java.lang.String;
 import java.util.Arrays;
 import java.lang.Math;
+import java.util.ArrayList;
+import static java.lang.System.out;
+
 
 
 
 public class Main {
+    public static void bubbleSort(int[] sortArr){
+        for (int i = 0; i < sortArr.length - 1; i++) {
+            for(int j = 0; j < sortArr.length - i - 1; j++) {
+                if(sortArr[j + 1] < sortArr[j]) {
+                    int swap = sortArr[j];
+                    sortArr[j] = sortArr[j + 1];
+                    sortArr[j + 1] = swap;
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) throws FileNotFoundException, IOException {
         String path = "INPUT.TXT";
         File file = new File(path);
         Scanner sc = new Scanner(file);
-        double A = (double)sc.nextInt();
-        double B = (double)sc.nextInt();
-        double C = (double)sc.nextInt();
-        double D = (double)sc.nextInt();
+        double A = sc.nextDouble();
+        double B = sc.nextDouble();
+        double C = sc.nextDouble();
+        double D = sc.nextDouble();
         String answer = "";
-        double p=(3*A*C-B*B)/(3*A*A);
-        double q=((2*B*B*B-9*A*B*C+27*A*A*D))/(27*A*A*A);
-        double s=(((q*q)/4)+(p*p*p)/27);
-        double f;
+        double p =(3*A*C-B*B)/(3*A*A);
+        double q=(2*B*B*B-9*A*B*C+27*A*A*D)/(27*A*A*A);
+        double discriminant = (((q*q)/4)+(p*p*p)/27);
+        double phi = 0;
+        //int[] Ar;
+        ArrayList<Integer> integers = new ArrayList<Integer>();
 
 
-        double discrim = 4*B*B - 12*A*C;
-        double toch_exstrem_X1;
-        double toch_exstrem_X2;
-        int vetka = -1;
-        if (discrim > 0){
-            toch_exstrem_X1 = (-2*B + Math.sqrt(discrim))/(6*A);
-            toch_exstrem_X2 = (-2*B - Math.sqrt(discrim))/(6*A);
-            //System.out.println(toch_exstrem_X1 + " toch_exstrem_X1");
-            //System.out.println(toch_exstrem_X2 + " toch_exstrem_X2");
-            double f_x1 = A*Math.pow(toch_exstrem_X1, 3) + B*Math.pow(toch_exstrem_X1, 2) + C*Math.pow(toch_exstrem_X1, 1) + D;
-            double f_x2 = A*Math.pow(toch_exstrem_X2, 3) + B*Math.pow(toch_exstrem_X2, 2) + C*Math.pow(toch_exstrem_X2, 1) + D;
-            if (f_x1 * f_x2 < 0) vetka = 3;
-            if (f_x1 * f_x2 == 0) vetka = 2;
-        }
-        else vetka = 1;
-
-        System.out.println(vetka + " kornya:");
-        System.out.println(s + " s:");
-        System.out.println(p + " p:");
-        System.out.println(q + " q:");
-        System.out.println(A + " A:");
-        System.out.println(B + " B:");
-        System.out.println(C + " C:");
-        System.out.println(D + " D:");
-
-
+        //System.out.println(cor_discr);
+        System.out.println(discriminant);
         if (q < 0){
-            f = (Math.atan(Math.pow(-s,0.5)/(-q/2)));
-            System.out.println(f + ": 1f");
+            phi = Math.atan(-1*Math.sqrt(-discriminant)/(q/2));
+            System.out.println("M");
         }
         if (q > 0){
-            f = Math.atan(Math.pow(-s,0.5)/(-q/2)) + Math.PI;
-            System.out.println(f + ": 2f");
+
+            phi = Math.atan(-1*Math.sqrt(-discriminant)/(q/2)) + Math.PI;
+            System.out.println("D: = ");
         }
-        else {
-            f = Math.PI/2;
-            System.out.println(f + ": 3f");
+        if (q == 0){
+            phi = Math.PI/2;
+            System.out.println("L:");
+        }
+        if(discriminant == 0){
+
+
+            int x1 = (int)Math.round(2*Math.pow(-q/2, 1/3) - B/(3*A));
+            int x2 = (int)Math.round(-1*Math.pow(-q/2, 1/3) - B/(3*A));
+                answer = x1 + " " + x2;
+            int[] Ar = {x1,x2};
+            integers.add(x1);
+            integers.add(x2);
+
+
         }
 
-        int x1, x2, x3;
-        double x2i, x3i;
+        if(discriminant > 0){
+            double y1 = Math.cbrt(-q/2 + Math.sqrt(discriminant)) + Math.cbrt(-q/2 - Math.sqrt(discriminant));
+            int x1 = (int)Math.round(y1 - B/(3*A));
+            int[] Ar = {x1};
+            integers.add(x1);
 
-        if (s<0)
-        {
-            x1=(int)Math.round(2*Math.sqrt(-p/3)*Math.cos(f/3)-B/3*A);
-            x2=(int)Math.round(2*Math.sqrt(-p/3)*Math.cos(f/3+(2*Math.PI)/3)-B/3*A);
-            x3=(int)Math.round(2*Math.sqrt(-p/3)*Math.cos(f/3+(4*Math.PI)/3)-B/3*A);
+        }
+        if(discriminant < 0){
+
+            double y1 = 2*Math.sqrt(-p/3)*Math.cos(phi/3);
+            double y2 = 2*Math.sqrt(-p/3)*Math.cos(phi/3+ 2*Math.PI/3);
+            double y3 = 2*Math.sqrt(-p/3)*Math.cos(phi/3+ 4*Math.PI/3);
+
+            int x1 = (int)Math.round(y1 - B/(3*A));
+            int x2 = (int)Math.round(y2 - B/(3*A));
+            int x3 = (int)Math.round(y3 - B/(3*A));
             answer = x1 + " " + x2 + " " + x3;
-        }
-        else if (s>0)
-        {
-            x1=(int)(Math.cbrt(-q/2+Math.pow(s,0.5))+Math.cbrt(-q/2-(Math.pow(s,0.5)))-B/(3*A));
-            answer = String.valueOf(x1);
+            int[] Ar = {x1,x2,x3};
+            integers.add(x1);
+            integers.add(x2);
+            integers.add(x3);
 
         }
-        else
-        {
-            x1=(int)Math.round(2*Math.cbrt(-q/2)-B/(3*A));
-            x2=(int)Math.round(-1*Math.cbrt(-q/2)-B/(3*A));
-            answer = x1 + " " + x2;
+        int []dsf = new int[integers.size()];
+        for (int i = 0; i < integers.size(); i++) {
+            dsf[i] = integers.get(i);
         }
 
+        bubbleSort(dsf);
 
-
+        if(discriminant < 0) {
+            answer = dsf[0] + " " + dsf[1] + " " + dsf[2];
+        }
+        if(discriminant == 0) {
+            answer = dsf[0] + " " + dsf[1];
+        }
+        if(discriminant > 0) {
+            answer = String.valueOf(dsf[0]);
+        }
         System.out.println(answer);
-
 
 
         FileWriter writerS = new FileWriter("OUTPUT.TXT", false);
